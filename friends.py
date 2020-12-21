c9a42626a9b2a35b0cb96b33c26445b415c22623
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2020-12-21 21:15:44 +0700

    Update friends.py

--- friends.py
+++ friends.py
@@ -1,11 +1,8 @@
-MAX = 20
-
-
-
 def findSet(u):
     while u != parent[u]:
         u = parent[u]
     return u
+
 
 def unionSet(u, v):
     up = findSet(u)
@@ -14,20 +11,21 @@ def unionSet(u, v):
         parent[up] = vp
         cnt[vp] += cnt[up]
 
+
 if __name__ == '__main__':
     t = int(input())
     for test in range(t):
-        n,m = map(int,input().split())
+        n, m = map(int, input().split())
         parent = [[] for l in range(n+1)]
-        for i in range(1,n+1):
+        for i in range(1, n+1):
             parent[i] = i
 
         cnt = [1 for i in range(n+1)]
         for j in range(m):
-            u,v = map(int,input().split())
-            unionSet(u,v)
+            u, v = map(int, input().split())
+            unionSet(u, v)
 
         ans = 0
-        for k in range(1,n+1):
-            ans = max(ans,cnt[i])
+        for k in range(1, n+1):
+            ans = max(ans, cnt[i])
         print(ans)
