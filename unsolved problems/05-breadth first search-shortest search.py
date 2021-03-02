83a8642f922f5c96e32f043504a8bfa64e7e5898
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2021-03-02 18:36:15 +0700

    Update 05-breadth first search-shortest search.py

--- unsolved problems/05-breadth first search-shortest search.py
+++ unsolved problems/05-breadth first search-shortest search.py
@@ -9,49 +9,50 @@
 
 # Độ phức tạp: O ( T * (V + E)) với T là số lượng test, V là số lượng đỉnh trong đồ thị và EE là số lượng cạnh trong đồ thị.
 
+
 import queue
- 
+
 MAX = 1000 + 5
 visited = [False] * MAX
 dist = [0] * MAX
 graph = [[] for i in range(MAX)]
- 
+
 def BFS(s):
     q = queue.Queue()
     visited[s] = True
     q.put(s)
- 
+
     while not q.empty():
         u = q.get()
- 
+
         for v in graph[u]:
             if not visited[v]:
                 visited[v] = True
                 dist[v] = dist[u] + 1
                 q.put(v)
- 
+
 Q = int(input())
- 
+
 for _ in range(Q):
     V, E = map(int, input().split())
- 
-    for i in range(MAX):
+
+    for i in range(MAX):        #reset graph
         graph[i].clear()
         visited[i] = False
         dist[i] = 0
-     
+
     for i in range(E):
         u, v = map(int, input().split())
         graph[u].append(v)
         graph[v].append(u)
-     
+
     s = int(input())
     BFS(s)
- 
+
     for i in range(1, V + 1):
         if i == s:
             continue
-         
+
         print(dist[i] * 6 if visited[i] else -1, end=' ')
- 
+
     print()
