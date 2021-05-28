5cd418f14393f4d452b1d6d50609632ce33a7668
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2021-05-29 00:45:24 +0700

    Create lc_remove-duplicates-from-sorted-array.py

---
+++ Leetcode/Easy/lc_remove-duplicates-from-sorted-array.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # nums[:] = sorted(list(set(nums)))
+        # return len(nums)
+
+        # for i in reversed(range(1, len(nums))):
+        #     if nums[i] == nums[i - 1]:
+        #         nums.pop(i)
+        # return len(nums)
+
+        #Two pointer
+        #i as the slow run (unique pointer) and j as the fast run (current pointer).
+        #Time complexity: O(n)
+        #Space complexity: O(1)
+        if len(nums) == 0:
+            return 0
+        i = 0
+        for j in range(1,len(nums)):
+            if nums[j] != nums[i]:
+                i+=1
+                nums[i] = nums[j]
+        return i+1
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.removeDuplicates([0, 1, 1, 2, 3, 4, 4]))
+
