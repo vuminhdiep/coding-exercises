dd9f8b682e248d9fe1f3e15290bc15cb51444b7c
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2021-08-25 02:00:02 +0700

    Create lc_search-insert-position.py

---
+++ Leetcode/Easy/lc_search-insert-position.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.searchInsert([1], 0))
+
