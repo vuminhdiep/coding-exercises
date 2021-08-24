72aa34a32951ddc8337b153df78e57bb8041a251
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2021-08-25 02:00:09 +0700

    Update lc_remove-element.py

--- Leetcode/Easy/lc_remove-element.py
+++ Leetcode/Easy/lc_remove-element.py
@@ -5,3 +5,15 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        count = 0
+        for num in nums:
+            if num != val:
+                nums[count] = num
+                count += 1
+        return count
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.removeElement([0, 1, 1, 2, 3, 4, 4], 4))
+
+
