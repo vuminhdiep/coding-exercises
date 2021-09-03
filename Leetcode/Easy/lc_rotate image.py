444750340312c266dd58c1c21d548fd6e869a1f4
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2021-09-04 00:01:54 +0700

    Create lc_rotate image.py

---
+++ Leetcode/Easy/lc_rotate image.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+
+        matrix.reverse()
+
+        #transpose
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        return matrix
