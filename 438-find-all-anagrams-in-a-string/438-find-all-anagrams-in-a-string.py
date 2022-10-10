a45407c5fe7f82d566fe04a10c1f1563286fdbbe
Vu Minh Diep <51457124+vuminhdiep@users.noreply.github.com>
2022-10-09 22:11:05 -0700

    Time: 84 ms (95.20%), Space: 14.6 MB (27.94%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -5,24 +5,10 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
-        #Sliding window
+
         #Constraints: just all consist of lowercase english
         #length of s and p >= 1
-        #hashmap or set to keep track of string p in s
-        #{a: 1, b: 1, c: 1}
-        #s = "cbaebabacd", p = "abc"
-        #.       i
-        #.    j
-        #i - j = len(p)
-        #if s[j:i] in hashmap -> add j to output array
-        #increment i, j
-        
-        #"abcab" {a: 1, b: 1}
-        #      i
-         #   j 
-            #if i == len(s) - 1:
-            #check s[i - len(p) : i] in hashmap
-            #i out ouf bound -> i - j < p
+        #hashmap or set to keep track of string p in s 
         #Use sliding window, check if hashmap_s == hashmap_p
         #pop the first added element in hashmap_s and add another element in hashmap_s as loop through string s to make sure hashmap_s always has the same number of key:value pairs as hashmap_p
         if len(s) < len(p):
@@ -31,7 +17,7 @@ class Solution(object):
         hashmap_p = {}
         hashmap_s = {}
         
-        #loop through string p and add to hashmap, initialize two hashmaps
+        #loop through string p and add to hashmap, initialize two hashmaps with the substring length p
         for i in range(len(p)):
             if p[i] not in hashmap_p:
                 hashmap_p[p[i]] = 1
@@ -43,22 +29,22 @@ class Solution(object):
             else:
                 hashmap_s[s[i]] += 1
         if hashmap_s == hashmap_p:
-            result = [0]
+            result = [0] #this means the first substring s is an anagram of string p so we add 0 index
         else:
             result = []
         left = 0
-        for right in range(len(p), len(s)):
+        for right in range(len(p), len(s)): #check from the second substring at index len(p) because the first substring already checked above
             if s[right] not in hashmap_s:
                 hashmap_s[s[right]] = 1
             else:
                 hashmap_s[s[right]] += 1
-            hashmap_s[s[left]] -= 1
+            hashmap_s[s[left]] -= 1 #decrement the left pointer as we add more character from right pointer to the hashmap, to keep the hashmap_s has same length as hashmap_p
             
-            if hashmap_s[s[left]] == 0:
-                hashmap_s.pop(s[left])
-            left += 1
+            if hashmap_s[s[left]] == 0: 
+                hashmap_s.pop(s[left]) #delete the first added element in hashmap_s to keep hashmap_s hash same length as hashmap_p -> to have space add s[right] to hashmap_s
+            left += 1 #increment left pointer to keep window distance of right - left = len(p)
             
             if hashmap_s == hashmap_p:
-                result.append(left)
+                result.append(left) #if two hashmaps are equal which means substring s has anagram of p, add the index of left pointer to result array
         return result
             
